a06c73ca4a531631d8ac9e1bdb544b4ec0bdc10e
David Kinder <david.kinder@david.kinder>
2020-10-06 11:44:37 +0100

    Level9 4.0

---
+++ Win/Classlib/dialog.h
@@ -0,0 +1,23 @@
+// Dialog.h
+
+class Dialog : public Object {
+	DLGPROC lpProcDialog;
+	int ID;
+public:
+	BOOL IsModal;
+	char *Section;
+	Dialog(Object *,int,char *Sect=NULL);
+	virtual ~Dialog();
+	int Execute();
+	virtual BOOL SetupWindow();
+	virtual void Paint(HDC, BOOL, RECT &);
+	virtual void EndDialog(int Result);
+	BOOL Create();
+	void KillDialog(int);
+	void SaveDialogPos();
+	void GetDialogPos();
+	void CentreDlgOnParent();
+
+	BOOL ChildIDMessage(UINT id ,TMSG &);
+	BOOL ChildMessage(HWND hw,TMSG &);
+};
---
+++ Win/Resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Level9.rc
+//
+#define IDR_MANIFEST                    1
+#define IDD_ABOUT                       100
+#define IDI_ICON                        101
+#define IDM_MENU                        102
+#define CM_OPEN                         200
+#define CM_FILELOAD                     201
+#define CM_FILESAVE                     202
+#define CM_DICTIONARY                   203
+#define CM_EXIT                         204
+#define CM_FONT                         205
+#define CM_HELPCONTENTS                 206
+#define CM_ABOUT                        207
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        208
+#define _APS_NEXT_COMMAND_VALUE         40000
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           100
+#endif
+#endif
